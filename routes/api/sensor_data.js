541f5302d8b7b632d69fa251d13f146e19cd5c1d
Rasa Cosmin <rasa.cosmin@gmail.com>
2019-01-08 14:46:32 +0200

    add new methods

--- routes/api/sensor_data.js
+++ routes/api/sensor_data.js
@@ -6,8 +6,14 @@ const Sensor = require('../../models/SensorData');
 router.get('/', (req, res) => {
 	Sensor.find()
 		.sort({ date: -1 })
-		.then(data => res.json(data))
-		.catch(err => res.status(404).json({ error: 'Nu exista date pentru senzor' }));
+		.then(s => {
+			const processed = s.map(it => {
+				return processSensorData(it);
+			});
+
+			res.json(processed);
+		})
+		.catch(err => res.status(404).json({ error: 'Nu exista date pentru senzor' + err }));
 });
 
 router.post('/temp', (req, res) => {
@@ -28,17 +34,61 @@ router.post('/hum', (req, res) => {
 	newData.save().then(sensor => res.json(sensor));
 });
 
+router.post('/', (req, res) => {
+	const newTemp = new Sensor({
+		value: req.body.temp_value,
+		type: 'temp'
+	});
+
+	const newHum = new Sensor({
+		value: req.body.hum_value,
+		type: 'hum'
+	});
+
+	newTemp.save().then(() => {
+		newHum.save().then(() => {
+			res.json({ message: 'success' });
+		});
+	});
+});
+
 router.get('/temp', (req, res) => {
 	Sensor.find({ type: 'temp' })
 		.sort({ date: -1 })
-		.then(s => res.json(s))
+		.then(s => {
+			const processed = s.map(it => {
+				return processSensorData(it);
+			});
+
+			res.json(processed);
+		})
 		.catch(err => res.status(404).json({ error: 'Nu exista date pentru senzor' }));
 });
 router.get('/hum', (req, res) => {
 	Sensor.find({ type: 'hum' })
 		.sort({ date: -1 })
-		.then(s => res.json(s))
+		.then(s => {
+			const processed = s.map(it => {
+				return processSensorData(it);
+			});
+
+			res.json(processed);
+		})
 		.catch(err => res.status(404).json({ error: 'Nu exista date pentru senzor' }));
 });
+
+const toTimestamp = str => {
+	var data = Date.parse(str);
+	return data / 1000;
+};
+
+const processSensorData = sensor => {
+	return {
+		id: sensor._id,
+		type: sensor.type,
+		value: sensor.value,
+		timestamp: toTimestamp(sensor.date)
+	};
+};
 
 module.exports = router;
